76a634f5359e7db58b54ab415cb2951145f20061
Did Intern <vikrambanand@gmail.com>
2022-12-10 12:11:11 +0000

    Added landing page to router file

--- client/src/router.js
+++ client/src/router.js
@@ -1,5 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import config from './config'
+
+import App from './App'
 import fetch from 'node-fetch'
 Vue.use(Router)
+
+const router =  new Router({
+    mode: 'hash',
+    routes: [
+      {
+        path: '/',
+        redirect: '/App',
+        requiresAuth:true,
+      }
+    ]
+})
